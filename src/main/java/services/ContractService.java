c5bb3b392e02b50b131e03c951c9aa9639c64c2b
Péricles Santos <pericles.programador@gmail.com>
2024-07-27 19:58:42 -0300

    Implementação basica dos serviços

---
+++ src/main/java/services/ContractService.java
@@ -0,0 +1,23 @@
+package services;
+
+import entities.Contract;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public OnlinePaymentService getOnlinePaymentService() {
+        return onlinePaymentService;
+    }
+
+    public void setOnlinePaymentService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, Integer months) {
+
+    }
+}
